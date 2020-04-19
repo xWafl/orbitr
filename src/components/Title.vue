af0732d4689f8a9575a420982e924176faec3072
xWafl <73sampleperson@gmail.com>
2020-04-19 16:04:07 -0400

    :sparkles: Make it all aligned and pretty

--- src/components/Title.vue
+++ src/components/Title.vue
@@ -4,25 +4,25 @@
         <div class="main">
             <div class="info">
                 <p v-if="!levelup && !gameOver">
-                    In this game, you are a spaceship orbiting around a star. You have to dodge the fireballs that are being hurtled towards you. To speed up or slow your ship, hold the mouse button. When you dodge 30 fireballs, you will advance to the next level.
+                    In this game, you are a spaceship orbiting around a star. You have to dodge the fireballs that are
+                    being hurtled towards you. To speed up or slow your ship, hold the mouse button. When you dodge 30
+                    fireballs, you will advance to the next level.
                 </p>
                 <p v-if="gameOver && score >= scoreThresh">
                     Great job! You beat level {{ level + 1 }} in {{ time }} seconds.
                 </p>
                 <p v-if="gameOver && score < scoreThresh">
-                    Oh no! You lost. You dodged {{ score }} bullets. Try this level again!
+                    Oh no! You lost. You dodged {{ score }}/30 bullets. Try this level again!
                 </p>
             </div>
-            <div class="buttons">
-                <div class="play" @click="play">
-                    {{ gameOver && (score &lt; scoreThresh) ? "Try again" : "Play" }}
-                </div>
-                <a href="https://github.com/xWafl/ludumdaregame" target="_blank">
-                    <div class="github">
-                        Github
-                    </div>
-                </a>
+            <div class="play" @click="play">
+                {{ gameOver && (score &lt; scoreThresh) ? "Try again" : "Play" }}
             </div>
+            <a href="https://github.com/xWafl/ludumdaregame" target="_blank" class="github">
+                <div>
+                    Github
+                </div>
+            </a>
         </div>
     </div>
 </template>
@@ -37,13 +37,13 @@
             level: Number,
             scoreThresh: Number,
             gameOver: Boolean,
-            started: Boolean
+            started: Boolean,
         },
         methods: {
             play () {
                 this.$emit("play", this.started && this.gameOver);
-            }
-        }
+            },
+        },
     }
 </script>
 
@@ -51,6 +51,7 @@
     .title {
         color: white;
     }
+
     .header {
         font-size: 72px;
         background-color: #2c3e50;
@@ -59,52 +60,51 @@
         border-radius: 10px;
         line-height: 200%;
     }
+
     .main {
-        display: flex;
+        display: grid;
+        grid-template-rows: repeat(20, 5%);
+        grid-template-columns: repeat(20, 5%);
         height: 50%;
-        align-items: center;
-    }
-    .info {
-        width: 40%;
+        width: 90%;
         margin: 5%;
+    }
+
+    .info {
+        grid-row: 1 / 21;
+        grid-column: 1 / 10;
+        width: 100%;
+        height: 100%;
         display: flex;
         font-size: 30px;
         background-color: #2c3e50;
         border-radius: 10px;
-        height: 90%;
         align-items: center;
     }
-    .buttons {
-        width: 40%;
-        margin: 5%;
-        display: inline-block;
-        font-size: 24px;
-        border-radius: 10px;
-        height: 100%;
-        user-select: none;
-    }
+
     .play {
+        grid-column: 11 / 21;
+        grid-row: 1 / 9;
         width: 100%;
-        margin: 5%;
-        display: inline-block;
+        height: 100%;
         font-size: 48px;
         font-weight: bold;
         background-color: #2c3e50;
         border-radius: 10px;
-        height: 40%;
-        line-height: 300%;
+        line-height: 350%;
         cursor: pointer;
     }
+
     .github {
+        grid-column: 11 / 21;
+        grid-row: 12 / 21;
+        height: 100%;
         width: 100%;
-        margin: 5%;
-        display: inline-block;
         font-size: 48px;
         font-weight: bold;
         background-color: #2c3e50;
         border-radius: 10px;
-        height: 40%;
-        line-height: 300%;
+        line-height: 350%;
         color: mediumpurple;
     }
 </style>
